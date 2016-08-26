e1e117a5bca87d9ccd61b6aae75c27fe300d1b0f
ThirdTime <league@Mac-mini.local>
2016-08-26 11:57:50 -0700

    Mazes and more!

--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -17,30 +17,40 @@ public class FlamingNinjaStar {
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 1. Make a new robot, and set it's pen down.
-
+Robot kennith = new Robot("batman");
+kennith.setX(800);
+kennith.setY(300);
+kennith.penDown();
+kennith.setPenColor(33, 44, 55);
 		// 12. Set the robot speed to 10
-
+kennith.setSpeed(3);
 		// 13. Make all the code below repeat 25 times
+for (int i = 0; i < 25; i++) {
+	
 
 			// 2. Turn the robot 1/8 of a circle
-		
+		kennith.turn(360/8);
 			// 3. Move the robot 64 pixels
-
-			
+kennith.move(64);
+			kennith.setPenColor(230, 0, 0);
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			kennith.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+			kennith.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+			kennith.turn(170);
+			kennith.setPenColor(33,44,55);
+
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			kennith.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
-			// 9. Move the robot the distance in the variable baseSize
-			
+kennith.turn(64);			
+// 9. Move the robot the distance in the variable baseSize
+			kennith.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
+			
 			// 11. Color your ninja star like Figure 2.
+}
 		
 	}
 
--- src/day5/StarShow.java
+++ src/day5/StarShow.java
@@ -9,29 +9,32 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class StarShow {
 	
-	Robot robot = new Robot("batman");
+	Robot bri = new Robot("batman");
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+		//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
-
+bri.setSpeed(10);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+int xPos = 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+int yPos = 950;
 		// 8. Make a variable to hold the star size and set it to 25
+int starSize = 25;
 
 		// 12. Repeat the steps #19 to #18, 30 times
-
-			// 19. Set the pen width to i 
+for (int i = 0; i < 30; i++) {
+	
+		// 19. Set the pen width to i 
 	
 			// 10. Set the X position of the robot to your X variable
-	
+	bri.setX(xPos);
+	bri.setY(yPos);
 			// 11. Set the Y position of the robot to your Y variable
 	
 			// 9. Call the drawStar() method with your star size variable
-	
+drawStar(starSize);	
 			// 14. Increase the X position by star size. See Figure 2.
 	
 			// 15. decrease the Y position by star size. See Figure 3.
@@ -41,18 +44,21 @@ public class StarShow {
 			// 17. Turn the robot 12 degrees
 	
 			// 18. Make each star a different random color like in Figure 4. 
-
+}
 	}
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+bri.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
+for (int i = 0; i < 5; i++) {
+	
 
 			// 1. Move the robot the distance of the starSize variable
-	
+bri.move(starSize);
 			// 3. Turn the robot 144 degrees
-			
+			bri.turn(144);
+}
 	}
 	
 	public static void main(String[] args) {
