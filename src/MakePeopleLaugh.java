e1f5f4e5a45853b038a3e9fb361d371b2f6f637a
ThirdTime <league@Mac-mini-3.local>
2016-10-15 11:29:09 -0700

    While Loops and Greeters

--- src/MakePeopleLaugh.java
+++ src/MakePeopleLaugh.java
@@ -7,6 +7,16 @@ public class MakePeopleLaugh {
 			dadJoke=dadJoke+2;
 JOptionPane.showMessageDialog(null, "Why do seagulls fly over the sea?");
 JOptionPane.showMessageDialog(null, "Because if they flew over the bay, they would be called bay-gulls");
+
+//How do "while loops" work?
+	//An explanation by a very forgetful programmer
+//Step 1; Create a variable, say "dadJoke". Set the value to a number, say "1"
+//Step 2; Decide how many times you want the thing to happen, say four times.
+//Create a While loop, "while(dadJoke(the variable)<=4(the number of times you want the thing to happen)){
+					//The thing happens
+					//dadJoke=dadJoke+1
+					//}
+
 	}
 	}
 }
--- src/WhileLoopChallenge.java
+++ src/WhileLoopChallenge.java
@@ -1,23 +1,48 @@
 
 public class WhileLoopChallenge {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 	
+int count=1;
 
+while(count<=3){
+System.out.print(count+",");	
+count=count+1;
+
+}
+System.out.println("4");
+
+System.out.println("\n");
 
 int num=-9;
 	
-	while(num<=-5){
+while(num<=-6){
 	num=num+1;
 	System.out.print(num);
-	}
+}
+	System.out.println("-4");
+System.out.println("\n");
+
+
+	int ber=12;
 	
-	
-	int ber=8;
-	
-	while(ber>=5){
-		ber=ber-1;
+	while(ber>=6){
+		ber=ber-2;
 	System.out.print(ber);
 	
+	
 	}
-}
-}
+	System.out.println("\n");
+	
+	int iveranoutofvariablenamessoletscallthiskennith=2;
+	
+	while(iveranoutofvariablenamessoletscallthiskennith<=16){
+		
+	System.out.print(iveranoutofvariablenamessoletscallthiskennith);
+	iveranoutofvariablenamessoletscallthiskennith=iveranoutofvariablenamessoletscallthiskennith*2;
+	
+	}
+	}
+	
+	}
+
+
